705747d6fcac67a8733ad8595a8b71616cbd4113
Sahar Bittman <saharr449@gmail.com>
2024-12-22 21:02:14 +0200

    add step to check if vpc exist

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ variable "region" {
 
 #Terraform Block
 terraform {
-  required_version = "~> 1.5.0" 
+  required_version = "~> 1.9.0" 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
